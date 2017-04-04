3d1e37795fced9cceb192f812058ce4f07e65bbe
jhennen <jhennen>
2017-04-04 15:20:02 -0500

    Changes to comments

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var http = require("http");
 var fs = require("fs");
 var queryString = require("querystring");
 var fileVar = "./doc_root/SampleTweets.json";
-var uneditedVar = "\r\n      {\r\n         \"text\":\"THE WORKSHOP TWITTER MICROSERVICE SERVER.JS CODE HAS NOT BEEN EDITED.\",\r\n         \"" +
-	"user\":{\r\n            \"screen_name\":\"**\"\r\n         },\r\n         \"entities\":{\r\n            " + 
-	"\"hashtags\":[{\r\n               \"text\":\"**\"}]},\r\n         \"timestamp_ms\":\"0000000000000\"\r\n      }";
 var jsonObj;
 
 // READ UNFILTERED JSON FILE AND PARSE IT INTO A JSON OBJECT.
@@ -21,6 +18,7 @@ response.writeHead(200, {
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
     'Content-Type': 'application/json'
 });
+
     // GET THE HASHTAG PARAMETER PASSED IN THE URL QUERY STRING.
     var queryVar = "ALL";
     if (request.url.indexOf('?' >= 0)) {
@@ -35,13 +33,9 @@ response.writeHead(200, {
             }
         }       
     }
+    
     // GENERATE RETURN JSON OBJECT.
-    response.write("{\r\n   \"tweets\":[");  
-    /*********************************************************    
-    UNCOMMENT THE CODE SECTION BELOW FOR THE WORKSHOP - THIS SECTION ASSEMBLES AND RETURNS THE TWITTER DATA FOR ALL TWEETS THAT COMPLY WITH THE HASHTAG FILTER.      
-    *********************************************************/
-    /*
-    uneditedVar = "";    
+    response.write("{\r\n   \"tweets\":[");    
     var firstRow = true;
     for (var k in jsonObj.tweets) { 
       if (jsonObj.tweets[k].entities) {
@@ -58,12 +52,7 @@ response.writeHead(200, {
             } 
         }
       }
-    }   
-    */
-    /*********************************************************    
-    UNCOMMENT THE CODE SECTION ABOVE FOR THE WORKSHOP.    
-    *********************************************************/  
-    response.write(uneditedVar); // DOES NOTHING IF SECTION ABOVE IS UNCOMMENTED       
+    }          
     response.write("\r\n   ]\r\n}");        
     response.end();
 }).listen(process.env.PORT || 8003);
